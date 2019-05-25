888183651c3093bab1a744524f6b85da41fedd4e
Romulo Soares <37163123+RomuloLASS@users.noreply.github.com>
2019-05-25 17:25:56 -0300

    activity_nova_tarefa.xml feita

--- app/src/main/java/br/ufjf/dcc196/trab03/NovaTarefaActivity.java
+++ app/src/main/java/br/ufjf/dcc196/trab03/NovaTarefaActivity.java
@@ -2,12 +2,32 @@ package br.ufjf.dcc196.trab03;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class NovaTarefaActivity extends AppCompatActivity {
+
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nova_tarefa);
+
+        spinner = findViewById(R.id.spinner);
+
+        List<String> linguagens = new ArrayList<>(Arrays.asList("Fácil","Medio", "Sem","Difícil","Muito Díficil"));
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, linguagens );
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(dataAdapter);
+
+
     }
 }
